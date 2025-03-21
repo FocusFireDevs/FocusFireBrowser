60775d80e50e6d19c83bc6bbefe26496f05ea85f
FocusFire <alecjasonaltman@gmail.com>
2025-03-20 19:22:43 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,366 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.WebView1 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Tab1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CancelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem7 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem8 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem9 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem5 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem6 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.WebView2 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        Me.WebView3 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        Me.WebView4 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.WebView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WebView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WebView4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(0, 24)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button1.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button1.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(48, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(48, 24)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button2.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button2.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(48, 48)
+        Me.Button2.TabIndex = 2
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.Location = New System.Drawing.Point(96, 24)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button3.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button3.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(48, 48)
+        Me.Button3.TabIndex = 3
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
+        Me.Button4.Location = New System.Drawing.Point(144, 24)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button4.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button4.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(48, 48)
+        Me.Button4.TabIndex = 4
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'WebView1
+        '
+        Me.WebView1.AllowExternalDrop = True
+        Me.WebView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView1.BackColor = System.Drawing.SystemColors.Control
+        Me.WebView1.CreationProperties = Nothing
+        Me.WebView1.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView1.Location = New System.Drawing.Point(0, 72)
+        Me.WebView1.Margin = New System.Windows.Forms.Padding(0)
+        Me.WebView1.Name = "WebView1"
+        Me.WebView1.Size = New System.Drawing.Size(784, 490)
+        Me.WebView1.Source = New System.Uri("https://start.duckduckgo.com/", System.UriKind.Absolute)
+        Me.WebView1.TabIndex = 9
+        Me.WebView1.ZoomFactor = 1.0R
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.MenuStrip1.Font = New System.Drawing.Font("Microsoft JhengHei UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(0)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Tab1ToolStripMenuItem, Me.ToolStripMenuItem7, Me.ToolStripMenuItem4, Me.ToolStripMenuItem1})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(0)
+        Me.MenuStrip1.Size = New System.Drawing.Size(784, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Tab1ToolStripMenuItem
+        '
+        Me.Tab1ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.CancelToolStripMenuItem})
+        Me.Tab1ToolStripMenuItem.Font = New System.Drawing.Font("Gadugi", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Tab1ToolStripMenuItem.Name = "Tab1ToolStripMenuItem"
+        Me.Tab1ToolStripMenuItem.Padding = New System.Windows.Forms.Padding(25, 0, 25, 0)
+        Me.Tab1ToolStripMenuItem.Size = New System.Drawing.Size(96, 24)
+        Me.Tab1ToolStripMenuItem.Text = "Tab 1"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'CancelToolStripMenuItem
+        '
+        Me.CancelToolStripMenuItem.Name = "CancelToolStripMenuItem"
+        Me.CancelToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CancelToolStripMenuItem.Text = "Cancel"
+        '
+        'ToolStripMenuItem7
+        '
+        Me.ToolStripMenuItem7.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem8, Me.ToolStripMenuItem9})
+        Me.ToolStripMenuItem7.Font = New System.Drawing.Font("Gadugi", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripMenuItem7.Name = "ToolStripMenuItem7"
+        Me.ToolStripMenuItem7.Padding = New System.Windows.Forms.Padding(25, 0, 25, 0)
+        Me.ToolStripMenuItem7.Size = New System.Drawing.Size(96, 24)
+        Me.ToolStripMenuItem7.Text = "Tab 2"
+        '
+        'ToolStripMenuItem8
+        '
+        Me.ToolStripMenuItem8.Name = "ToolStripMenuItem8"
+        Me.ToolStripMenuItem8.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem8.Text = "Open"
+        '
+        'ToolStripMenuItem9
+        '
+        Me.ToolStripMenuItem9.Name = "ToolStripMenuItem9"
+        Me.ToolStripMenuItem9.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem9.Text = "Cancel"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem5, Me.ToolStripMenuItem6})
+        Me.ToolStripMenuItem4.Font = New System.Drawing.Font("Gadugi", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Padding = New System.Windows.Forms.Padding(25, 0, 25, 0)
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(96, 24)
+        Me.ToolStripMenuItem4.Text = "Tab 3"
+        '
+        'ToolStripMenuItem5
+        '
+        Me.ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        Me.ToolStripMenuItem5.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem5.Text = "Open"
+        '
+        'ToolStripMenuItem6
+        '
+        Me.ToolStripMenuItem6.Name = "ToolStripMenuItem6"
+        Me.ToolStripMenuItem6.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem6.Text = "Cancel"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem3})
+        Me.ToolStripMenuItem1.Font = New System.Drawing.Font("Gadugi", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Padding = New System.Windows.Forms.Padding(25, 0, 25, 0)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(96, 24)
+        Me.ToolStripMenuItem1.Text = "Tab 4"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem2.Text = "Open"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(116, 22)
+        Me.ToolStripMenuItem3.Text = "Cancel"
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button5.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Image = CType(resources.GetObject("Button5.Image"), System.Drawing.Image)
+        Me.Button5.Location = New System.Drawing.Point(736, 24)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button5.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button5.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(48, 48)
+        Me.Button5.TabIndex = 11
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button6.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Image = CType(resources.GetObject("Button6.Image"), System.Drawing.Image)
+        Me.Button6.Location = New System.Drawing.Point(688, 24)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button6.MaximumSize = New System.Drawing.Size(48, 48)
+        Me.Button6.MinimumSize = New System.Drawing.Size(48, 48)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(48, 48)
+        Me.Button6.TabIndex = 12
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'WebView2
+        '
+        Me.WebView2.AllowExternalDrop = True
+        Me.WebView2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView2.BackColor = System.Drawing.SystemColors.Control
+        Me.WebView2.CreationProperties = Nothing
+        Me.WebView2.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView2.Location = New System.Drawing.Point(0, 72)
+        Me.WebView2.Margin = New System.Windows.Forms.Padding(0)
+        Me.WebView2.Name = "WebView2"
+        Me.WebView2.Size = New System.Drawing.Size(784, 490)
+        Me.WebView2.Source = New System.Uri("https://start.duckduckgo.com/", System.UriKind.Absolute)
+        Me.WebView2.TabIndex = 13
+        Me.WebView2.ZoomFactor = 1.0R
+        '
+        'WebView3
+        '
+        Me.WebView3.AllowExternalDrop = True
+        Me.WebView3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView3.BackColor = System.Drawing.SystemColors.Control
+        Me.WebView3.CreationProperties = Nothing
+        Me.WebView3.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView3.Location = New System.Drawing.Point(0, 72)
+        Me.WebView3.Margin = New System.Windows.Forms.Padding(0)
+        Me.WebView3.Name = "WebView3"
+        Me.WebView3.Size = New System.Drawing.Size(784, 490)
+        Me.WebView3.Source = New System.Uri("https://start.duckduckgo.com/", System.UriKind.Absolute)
+        Me.WebView3.TabIndex = 14
+        Me.WebView3.ZoomFactor = 1.0R
+        '
+        'WebView4
+        '
+        Me.WebView4.AllowExternalDrop = True
+        Me.WebView4.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView4.BackColor = System.Drawing.SystemColors.Control
+        Me.WebView4.CreationProperties = Nothing
+        Me.WebView4.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView4.Location = New System.Drawing.Point(0, 72)
+        Me.WebView4.Margin = New System.Windows.Forms.Padding(0)
+        Me.WebView4.Name = "WebView4"
+        Me.WebView4.Size = New System.Drawing.Size(784, 490)
+        Me.WebView4.Source = New System.Uri("https://start.duckduckgo.com/", System.UriKind.Absolute)
+        Me.WebView4.TabIndex = 15
+        Me.WebView4.ZoomFactor = 1.0R
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.WebView4)
+        Me.Controls.Add(Me.WebView3)
+        Me.Controls.Add(Me.WebView2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.WebView1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(3840, 2160)
+        Me.MinimumSize = New System.Drawing.Size(800, 600)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FocusFire"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.WebView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.WebView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WebView3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WebView4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents WebView1 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents Tab1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents WebView2 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents WebView3 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents WebView4 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CancelToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem7 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem8 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem9 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem5 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem6 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebView1.GoBack()
+        WebView2.GoBack()
+        WebView3.GoBack()
+        WebView4.GoBack()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WebView1.GoForward()
+        WebView2.GoForward()
+        WebView3.GoForward()
+        WebView4.GoForward()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WebView1.Reload()
+        WebView2.Reload()
+        WebView3.Reload()
+        WebView4.Reload()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        WebView1.Source = New Uri("https://start.duckduckgo.com/")
+        WebView2.Source = New Uri("https://start.duckduckgo.com/")
+        WebView3.Source = New Uri("https://start.duckduckgo.com/")
+        WebView4.Source = New Uri("https://start.duckduckgo.com/")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ActiveForm.WindowState = FormWindowState.Maximized
+        ActiveForm.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ActiveForm.WindowState = FormWindowState.Normal
+        ActiveForm.FormBorderStyle = FormBorderStyle.None
+        ActiveForm.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        WebView2.Visible = False
+        WebView2.Enabled = False
+        WebView3.Visible = False
+        WebView3.Enabled = False
+        WebView4.Visible = False
+        WebView4.Enabled = False
+        WebView1.Enabled = True
+        WebView1.Visible = True
+    End Sub
+
+    Private Sub OpenToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem8.Click
+        WebView1.Visible = False
+        WebView1.Enabled = False
+        WebView3.Visible = False
+        WebView3.Enabled = False
+        WebView4.Visible = False
+        WebView4.Enabled = False
+        WebView2.Visible = True
+        WebView2.Enabled = True
+    End Sub
+
+    Private Sub OpenToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem5.Click
+        WebView1.Visible = False
+        WebView1.Enabled = False
+        WebView2.Visible = False
+        WebView2.Enabled = False
+        WebView4.Visible = False
+        WebView4.Enabled = False
+        WebView3.Visible = True
+        WebView3.Enabled = True
+    End Sub
+
+    Private Sub OpenToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        WebView1.Visible = False
+        WebView1.Enabled = False
+        WebView2.Visible = False
+        WebView2.Enabled = False
+        WebView3.Visible = False
+        WebView3.Enabled = False
+        WebView4.Visible = True
+        WebView4.Enabled = True
+    End Sub
+
+    Private Sub CancelToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CancelToolStripMenuItem.Click
+        Tab1ToolStripMenuItem.HideDropDown()
+    End Sub
+
+    Private Sub CancelToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem9.Click
+        Tab1ToolStripMenuItem.HideDropDown()
+    End Sub
+
+    Private Sub CancelToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem6.Click
+        Tab1ToolStripMenuItem.HideDropDown()
+    End Sub
+
+    Private Sub Tab4ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Tab1ToolStripMenuItem.HideDropDown()
+    End Sub
+End Class
